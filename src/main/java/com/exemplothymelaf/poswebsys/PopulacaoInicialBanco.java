a2a06264a34c9179cc63f5c5393e5cc556c6eacf
gabrielolimpio <gabriel.rochaleao@gmail.com>
2023-07-06 20:42:10 -0300

    Implementacao de listagem de pessosa

--- src/main/java/com/exemplothymelaf/poswebsys/PopulacaoInicialBanco.java
+++ src/main/java/com/exemplothymelaf/poswebsys/PopulacaoInicialBanco.java
@@ -1,9 +1,13 @@
 package com.exemplothymelaf.poswebsys;
 
+import com.exemplothymelaf.poswebsys.rh.dominio.Pessoa;
+import com.exemplothymelaf.poswebsys.rh.dominio.PessoaRepositorio;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 @Transactional
@@ -11,6 +15,20 @@ public class PopulacaoInicialBanco implements CommandLineRunner {
 
     @Autowired
     private PessoaRepositorio pessoaRepo;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Pessoa p1 = new Pessoa("Joao");
+        p1.setDataNascimento(LocalDate.of(1990, 6, 1));
+        p1.setEmail("joao@gmail.com");
+
+        Pessoa p2 = new Pessoa("Maria");
+        p1.setDataNascimento(LocalDate.of(1991, 8, 1));
+        p1.setEmail("maria@gmail.com");
+
+        pessoaRepo.save(p1);
+        pessoaRepo.save(p2);
+    }
 
 
 }
--- src/main/java/com/exemplothymelaf/poswebsys/rh/dominio/Pessoa.java
+++ src/main/java/com/exemplothymelaf/poswebsys/rh/dominio/Pessoa.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 public class Pessoa {
@@ -24,12 +25,79 @@ public class Pessoa {
     private String email;
     private String telefone;
 
+    @Deprecated
     protected Pessoa(){}
 
     public Pessoa(String nome) {
         this.nome = nome;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(id, pessoa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [" +
+                "nome='" + nome + '\'' +
+                ']';
+    }
 
 }
