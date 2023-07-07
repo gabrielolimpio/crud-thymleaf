15486d94ffb26f05debf7af8302942e9f5248449
Gabriel Olimpio <gabriel.rochaleao@gmail.com>
2023-07-07 14:12:22 -0300

    implementacao da alteracao e exclusao de pessoas

--- src/main/java/com/exemplothymelaf/poswebsys/rh/controle/PessoaControle.java
+++ src/main/java/com/exemplothymelaf/poswebsys/rh/controle/PessoaControle.java
@@ -6,7 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Controller
 public class PessoaControle {
@@ -28,10 +32,33 @@ public class PessoaControle {
         model.addAttribute("pessoa", new Pessoa(""));
         return "rh/pessoas/form";
     }
+    @GetMapping("/rh/pessoas/{id}")
+    public String alterarPessoa(@PathVariable("id") long id, Model model){
+
+        Optional<Pessoa> pessoaOpt = pessoaRepo.findById(id);
+        if(pessoaOpt.isEmpty()) {
+            throw new IllegalArgumentException("Pessoa invalida.");
+        }
+
+        model.addAttribute("pessoa", pessoaOpt.get());
+        return "rh/pessoas/form";
+    }
 
     @PostMapping("rh/pessoas/salvar")
     public String salvarPessoa(@ModelAttribute("pessoa") Pessoa pessoa){
         pessoaRepo.save(pessoa);
+        return "redirect:/rh/pessoas";
+    }
+
+    @GetMapping("/rh/pessoas/excluir/{id}")
+    public String excluirPessoa(@PathVariable("id") long id){
+        Optional<Pessoa> pessoaOpt = pessoaRepo.findById(id);
+        if(pessoaOpt.isEmpty()){
+            throw new IllegalArgumentException("Pessoa invalida.");
+        }
+
+        pessoaRepo.delete(pessoaOpt.get());
+
         return "redirect:/rh/pessoas";
     }
 }
