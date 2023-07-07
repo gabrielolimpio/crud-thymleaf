c5e9cfb6139937646552e920db608c04432958ce
Gabriel Olimpio <gabriel.rochaleao@gmail.com>
2023-07-07 08:45:45 -0300

    implementacao da inclusao de pessoas

--- src/main/java/com/exemplothymelaf/poswebsys/rh/controle/PessoaControle.java
+++ src/main/java/com/exemplothymelaf/poswebsys/rh/controle/PessoaControle.java
@@ -1,9 +1,12 @@
 package com.exemplothymelaf.poswebsys.rh.controle;
 
+import com.exemplothymelaf.poswebsys.rh.dominio.Pessoa;
 import com.exemplothymelaf.poswebsys.rh.dominio.PessoaRepositorio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PessoaControle {
@@ -18,5 +21,17 @@ public class PessoaControle {
     public String pessoas(Model model) {
         model.addAttribute("listaPessoas", pessoaRepo.findAll());
         return "rh/pessoas/index";
+    }
+
+    @GetMapping("/rh/pessoas/nova")
+    public String novaPessoa(Model model){
+        model.addAttribute("pessoa", new Pessoa(""));
+        return "rh/pessoas/form";
+    }
+
+    @PostMapping("rh/pessoas/salvar")
+    public String salvarPessoa(@ModelAttribute("pessoa") Pessoa pessoa){
+        pessoaRepo.save(pessoa);
+        return "redirect:/rh/pessoas";
     }
 }
